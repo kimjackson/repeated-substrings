5db6de0a3572067461ba0b10822804bc605d43da
Kim Jackson <kim.jackson@optus.com.au>
2018-08-20 17:56:51 +1000

    Compare using indices into the string rather than taking substrings.

--- RepeatedSubstrings.hs
+++ RepeatedSubstrings.hs
@@ -3,17 +3,21 @@ main = do
     print (repeatedsubstrings input)
 
 repeatedsubstrings :: String -> Int
-repeatedsubstrings s  = sum [ checkpartitions s i | i <- [1..((length s)-1) `div` 2] ]
+repeatedsubstrings s  = do
+    let l = length s
+    let m = (l-1) `div` 2
+    sum [ checkpartitions s l i | i <- [1..m] ]
 
-checkpartitions :: String -> Int -> Int
---                      YYW             YWY             WYY
-checkpartitions s i  = (match y1 y2) + (match y1 y4) + (match y3 y4)
-    where y1 = take i s                 -- 0..i       first i chars
-          y2 = take i $ drop i s        -- i..2i      next i chars
-          y3 = take i $ drop (l-2*i) s  -- l-2i..l-i  next to last i chars
-          y4 = drop (l-i) s             -- l-i..l     last i chars
-          l  = length s
+checkpartitions :: String -> Int -> Int -> Int
+checkpartitions s l i  = (match s l i 0 0 i)           -- YYW
+                       + (match s l i 0 0 (l-i))       -- YWY
+                       + (match s l i 0 (l-2*i) (l-i)) -- WYY
+-- 0..i       first i chars
+-- i..2i      next i chars
+-- l-2i..l-i  next to last i chars
+-- l-i..l     last i chars
 
-match :: String -> String -> Int
-match x y | x == y    = 1
-          | otherwise = 0
+match :: String -> Int -> Int-> Int -> Int -> Int -> Int
+match s l i j y1 y2 | j == i                 = 1
+                    | s!!(y1+j) /= s!!(y2+j) = 0
+                    | otherwise              = match s l i y1 y2 (j+1)
